c8bc83f05c21da40c042a104286d3d81add68b3e
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-09-15 18:16:50 +0700

    create table customer info

---
+++ database/migrations/2020_09_15_111209_create_table__customer_info.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableCustomerInfo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_info', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('address');
+            $table->string('phone_number');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table__customer_info');
+    }
+}
