be39d83bda58d318852bc455dd3f7eb6534d1642
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-09-16 10:13:41 +0700

    add create_table_notification done

---
+++ database/migrations/2020_09_16_031130_create_table_notifications.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableNotifications extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            //
+            $table->increments('id');
+            $table->string('message');
+            $table->boolean('status');
+            $table->date('create_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('notifications', function (Blueprint $table) {
+            //
+        });
+    }
+}
