54aacb9667f503b3bc9950c1733c4c6191beaea0
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-10-16 14:17:43 +0700

    add image to product

---
+++ database/migrations/2020_10_16_071548_add_images_to_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddImagesToProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+            $table->unsignedInteger('image_id')->after('createdDate')->nullable();
+            $table->foreign('image_id')->references('id')->on('images');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+}
