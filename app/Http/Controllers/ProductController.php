08ab11b27a739be057659ec9eb7427be3a9ca53a
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-10-16 08:07:26 +0700

    add product controller

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\ProductService;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    //
+    protected $productService;
+
+    public function __construct(ProductService $categoryService)
+    {
+        $this->productService = $categoryService;
+    }
+
+    public function index()
+    {
+        $products = $this->productService->getAll();
+        return response()->json($products, 200);
+    }
+
+    public function show($id)
+    {
+        $product = $this->productService->findById($id);
+        return response()->json($product['$products'], $product['statusCode']);
+    }
+
+    public function store(Request $request)
+    {
+        $dataProduct = $this->productService->create($request->all());
+
+        return response()->json($dataProduct['$products'], $dataProduct['statusCode']);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $dataProduct = $this->productService->update($request->all(), $id);
+
+        return response()->json($dataProduct['$products'], $dataProduct['statusCode']);
+    }
+
+    public function destroy($id)
+    {
+        $product = $this->productService->destroy($id);
+        return response()->json($product['message'], $product['statusCode']);
+    }
+}
