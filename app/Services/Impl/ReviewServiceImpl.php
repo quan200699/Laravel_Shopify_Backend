19582597c9625ab851a2d630de6cf222a1e50bd7
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-10-16 15:20:08 +0700

    add review service

---
+++ app/Services/Impl/ReviewServiceImpl.php
@@ -0,0 +1,88 @@
+<?php
+
+
+namespace App\Services\Impl;
+
+
+use App\Repositories\ReviewRepository;
+use App\Services\ReviewService;
+
+class ReviewServiceImpl implements ReviewService
+{
+    protected $reviewRepository;
+
+    public function __construct(ReviewRepository $reviewRepository)
+    {
+        $this->reviewRepository = $reviewRepository;
+    }
+
+    public function getAll()
+    {
+        return $this->reviewRepository->getAll();
+    }
+
+    public function findById($id)
+    {
+        $review = $this->reviewRepository->findById($id);
+        $statusCode = 200;
+        if (!$review) {
+            $statusCode = 404;
+        }
+        $data = [
+            'statusCode' => $statusCode,
+            'reviews' => $review
+        ];
+        return $data;
+    }
+
+    public function create($request)
+    {
+        $review = $this->reviewRepository->create($request);
+
+        $statusCode = 201;
+        if (!$review)
+            $statusCode = 500;
+
+        $data = [
+            'statusCode' => $statusCode,
+            'reviews' => $review
+        ];
+
+        return $data;
+    }
+
+    public function update($request, $id)
+    {
+        $oldReview = $this->reviewRepository->findById($id);
+        if (!$oldReview) {
+            $statusCode = 404;
+            $newReview = null;
+        } else {
+            $newReview = $this->reviewRepository->update($request, $oldReview);
+            $statusCode = 200;
+        }
+        $data = [
+            'statusCode' => $statusCode,
+            'reviews' => $newReview
+        ];
+        return $data;
+    }
+
+    public function destroy($id)
+    {
+        $review = $this->reviewRepository->findById($id);
+        $statusCode = 200;
+        if (!$review) {
+            $statusCode = 404;
+            $message = "NOT FOUND";
+        } else {
+            $this->reviewRepository->destroy($review);
+            $message = "DELETE SUCCESS";
+        }
+        $data = [
+            'statusCode' => $statusCode,
+            'message' => $message
+        ];
+        return $data;
+    }
+}
---
+++ app/Services/ReviewService.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Services;
+
+
+interface ReviewService extends Service
+{
+
+}
