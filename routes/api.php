c5aea53c352e64ecbe36cca2942c699ace81e245
hoangvinhtien123 <55055765+hoangvinhtien123@users.noreply.github.com>
2020-10-17 19:53:14 +0700

    fix api

--- routes/api.php
+++ routes/api.php
@@ -19,15 +19,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 Route::group(['prefix' => 'warehouses'], function () {
     Route::get('/', 'WarehouseController@index')->name('warehouse.all');
-    Route::get('/{customerId}', 'WarehouseController@show')->name('warehouse.show');
+    Route::get('/{id}', 'WarehouseController@show')->name('warehouse.show');
     Route::post('/', 'WarehouseController@store')->name('warehouse.store');
-    Route::put('/{customerId}', 'WarehouseController@update')->name('warehouse.update');
-    Route::delete('/{customerId}', 'WarehouseController@destroy')->name('warehouse.destroy');
+    Route::put('/{id}', 'WarehouseController@update')->name('warehouse.update');
+    Route::delete('/{id}', 'WarehouseController@destroy')->name('warehouse.destroy');
 });
 Route::group(['prefix' => 'warehouse_bills'], function () {
     Route::get('/', 'WarehouseBillController@index')->name('warehouseBill.all');
-    Route::get('/{customerId}', 'WarehouseBillController@show')->name('warehouseBill.show');
+    Route::get('/{id}', 'WarehouseBillController@show')->name('warehouseBill.show');
     Route::post('/', 'WarehouseBillController@store')->name('warehouseBill.store');
-    Route::put('/{customerId}', 'WarehouseBillController@update')->name('warehouseBill.update');
-    Route::delete('/{customerId}', 'WarehouseBillController@destroy')->name('warehouseBill.destroy');
+    Route::put('/{id}', 'WarehouseBillController@update')->name('warehouseBill.update');
+    Route::delete('/{id}', 'WarehouseBillController@destroy')->name('warehouseBill.destroy');
 });
