02848cdc154a5a3f8327468a30ea400411edbad0
hoangvinhtien123 <55055765+hoangvinhtien123@users.noreply.github.com>
2020-10-19 13:55:20 +0700

    fix repo implement

--- app/Repositories/Impl/WarehouseBillDetailRepositoryImpl.php
+++ app/Repositories/Impl/WarehouseBillDetailRepositoryImpl.php
@@ -4,14 +4,14 @@
 namespace App\Repositories\Impl;
 
 
-use App\Image;
+use App\WarehouseBillDetail;
 
 class WarehouseBillDetailRepositoryImpl extends \App\Repositories\Eloquent\EloquentRepository implements \App\Repositories\WarehouseBillDetailRepository
 {
 
     public function getModel()
     {
-        $model = Image::class;
+        $model = WarehouseBillDetail::class;
         return $model;
     }
 }
