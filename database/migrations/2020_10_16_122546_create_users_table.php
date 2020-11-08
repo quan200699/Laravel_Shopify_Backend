5e64655b7c0238f9efd05cae81fd6f42e287956c
quan200699 <51079189+quan200699@users.noreply.github.com>
2020-11-08 23:29:12 +0700

    fix user table

--- database/migrations/2020_10_16_122546_create_users_table.php
+++ database/migrations/2020_10_16_122546_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('facebook_id')->nullable();
-            $table->bigInteger('google_id')->nullable();
+            $table->string('google_id')->nullable();
             $table->string('email')->unique();
             $table->string('fullName')->nullable();
             $table->string('password')->nullable();
